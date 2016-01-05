36022fb8f884d21729db95a936a01d7c7799dc8f
wmzy <1256573276@qq.com>
2016-01-05 18:17:09 +0800

    parse to array

--- index.js
+++ index.js
@@ -15,42 +15,31 @@ var cheerio = require('cheerio');
 
 function makeContentTable() {
 	var linkTemplate = _.template('[${ title }](${ path })');
-	var preface = [];
-	var contentTable = [];
 	fs.readFile(path.join(__dirname, 'abs-3.9.1_cn', 'index.html'), function (err, data) {
 		if (err) console.error(err);
 
 		var html = iconv.decode(data, 'GBK');
 		var $ = cheerio.load(html);
-		var $contentTable = $('.TOC > dl');
 
-		parseDl($contentTable)
+		// 目录
+		var contentTable = parseDl($('.TOC > dl'));
 
-		// 移除‘目录’节点
-		$contentTable.children().first().remove();
+		// 表格清单
+		var tableList = parseDl($('.LOT > dl').first());
 
-		// 序
-		var $preface = $contentTable.children().first().remove();
-		var $prefaceItems = $contentTable.children().first().remove().find('dt:has(a)');
-		preface.push({title: $preface.text(), path: $preface.find('a').attr('href')});
-		$prefaceItems.each(function () {
-			preface.push({title: $(this).text(), path: $(this).find('a').attr('href')});
-		});
-
-		console.log(preface);
-
-		// todo:参考文献
+		// 例子清单
+		var exampleList = parseDl($('.TOC > dl').last());
 
 
-		// todo:目录
-
-		console.log($contentTable.find('dt:has(a)').first().text());
 		function parseDl($dl) {
-			return $dl.children().map(function (i, el) {
-				if (el.name === dt) return parseDt(el);
+			var res = [];
 
-				if (el.name === 'dd') return parseDl($(el).children().first());
-			})
+			$dl.children().each(function (i, el) {
+				if (el.name === 'dt') return res.push(parseDt($(el)));
+				if (el.name === 'dd') return res.push(parseDl($(el).children().first()));
+			});
+
+			return res;
 		}
 
 		function parseDt($dt) {
